3f0f1fac3b3471002ecf77b3e186dad89d52966d
David <forexsencillo@gmail.com>
2024-09-21 01:25:48 +0200

    Cosis

--- src/Main.kt
+++ src/Main.kt
@@ -1,113 +1,130 @@
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Random;
-import java.time.Duration;
-import java.time.Instant;
+import java.io.FileWriter
+import java.io.IOException
+import java.sql.Time
+import java.time.Duration
+import java.time.Instant
+import java.util.*
+import kotlin.concurrent.timer
+import kotlin.system.measureNanoTime
+import kotlin.system.measureTimeMillis
+
+internal object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val TAMVECTOR = 10000
+        var vector: IntArray
+        val mediciones = DoubleArray(10)
+
+        var tiempo: Long
 
 
-class Main {
-    public static void main(String[] args) {
-
-        final int TAMVECTOR = 10000;
-        int[] vector;
-        double[] mediciones = new double[20];
-        Instant start, finish;
+        //        String cadenaCSV = stringACSV(intAStringArray(a));
 
 
-//        String cadenaCSV = stringACSV(intAStringArray(a));
-
+        repeat(3) { it0 ->
 
 //        for (int nAlgoritmo = 0; nAlgoritmo < 3; nAlgoritmo++) {
+            for (i in 0..9) {
+                vector = IntArray(TAMVECTOR * (i + 1))
+                println(vector.size)
 
-            for (int i = 1; i < 10; i++) {
-                vector = new int[TAMVECTOR * i];
-                System.out.println(vector.length);
+                repeat(20) {
+                    aleatorio(vector, TAMVECTOR)
 
-                for (int j = 0; j < 20; j++) {
-                    aleatorio(vector, TAMVECTOR);
+                    tiempo = when (it0) {
+                        0 -> measureNanoTime {
+                            ordena1(vector, TAMVECTOR)
+                        }
 
-                    start = Instant.now();
+                        1 -> measureNanoTime {
+                            ordena2(vector, TAMVECTOR)
+                        }
 
-                    ordena3(vector, TAMVECTOR);
+                        else -> measureNanoTime {
+                            ordena3(vector, TAMVECTOR)
+                        }
+                    }
 
-                    finish = Instant.now();
-                    mediciones[j] = (double) Duration.between(start, finish).toNanos() / 1000000000;
+
+                    mediciones[i] += tiempo.toDouble()
                 }
 
-                System.out.println(Arrays.toString(mediciones));
-                escribirCSV(stringACSV(intAStringArray(mediciones)), 1);
+                mediciones[i] = mediciones[i] / 20
 
             }
+            println(mediciones.contentToString())
+            escribirCSV(stringACSV(intAStringArray(mediciones)), 0)
         }
-//    }
+    }
 
-    private static void escribirCSV(String cad, int archivo) {
+    //    }
+    private fun escribirCSV(cad: String, archivo: Int) {
         try {
-            FileWriter myWriter = new FileWriter("src/algoritmo" + archivo + ".csv", true);
-            myWriter.write(cad + "\n");
-            myWriter.close();
-            System.out.println("Todo bn.");
-        } catch (IOException e) {
-            System.out.println("ERROR CATASTRÓFICO.");
-            e.printStackTrace();
+            val myWriter = FileWriter("src/tiempos/algoritmo$archivo.csv", true)
+            myWriter.write(cad + "\n")
+            myWriter.close()
+//            println("Todo bn.")
+        } catch (e: IOException) {
+            println("ERROR CATASTRÓFICO.")
+            e.printStackTrace()
         }
     }
 
 
-    private static String[] intAStringArray(double[] intArray) {
-        String[] nuevoArray = new String[intArray.length];
+    private fun intAStringArray(intArray: DoubleArray): Array<String?> {
+        val nuevoArray = arrayOfNulls<String>(intArray.size)
 
-        for (int i = 0; i < intArray.length; i++) {
-            nuevoArray[i] = String.valueOf(intArray[i]);
+        for (i in intArray.indices) {
+            nuevoArray[i] = intArray[i].toString()
         }
-        return nuevoArray;
+        return nuevoArray
     }
 
-    private static String stringACSV(String[] array) {
-        return String.join(",", array);
+    private fun stringACSV(array: Array<String?>): String {
+        return java.lang.String.join(",", *array)
     }
 
-    private static void ordenarSegunAlgoritmo(int[] vector, int tam, int algoritmo) {
+    private fun ordenarSegunAlgoritmo(vector: IntArray, tam: Int, algoritmo: Int) {
         if (algoritmo == 0) {
-            ordena1(vector, algoritmo);
+            ordena1(vector, algoritmo)
         } else if (algoritmo == 1) {
-            ordena2(vector, algoritmo);
+            ordena2(vector, algoritmo)
         } else if (algoritmo == 2) {
-            ordena3(vector, algoritmo);
+            ordena3(vector, algoritmo)
         }
     }
 
-// Medir el tiempo
-//import java.time.Duration;
-//import java.time.Instant;
-//...
-//Instant start = Instant.now();
-//// Código que queremos medir
-//Instant finish = Instant.now();
-//long timeElapsed = Duration.between(start, finish).toNanos();
 
-
+    // Medir el tiempo
+    //import java.time.Duration;
+    //import java.time.Instant;
+    //...
+    //Instant start = Instant.now();
+    //// Código que queremos medir
+    //Instant finish = Instant.now();
+    //long timeElapsed = Duration.between(start, finish).toNanos();
     // A
-    static Random random = new Random();
+    var random: Random = Random()
 
 
     // Alg1
-    public static void ordena1(int v[], int tam) {
-        int i, j, temp;
-        i = 1;
-        j = 2;
+    fun ordena1(v: IntArray, tam: Int) {
+        var i: Int
+        var j: Int
+        var temp: Int
+        i = 1
+        j = 2
         while (i < tam) {
             if (v[i - 1] <= v[i]) {
-                i = j;
-                j = j + 1;
+                i = j
+                j = j + 1
             } else {
-                temp = v[i - 1];
-                v[i - 1] = v[i];
-                v[i] = temp;
-                i = i - 1;
+                temp = v[i - 1]
+                v[i - 1] = v[i]
+                v[i] = temp
+                i = i - 1
                 if (i == 0) {
-                    i = 1;
+                    i = 1
                 }
             }
         }
@@ -115,88 +132,93 @@ class Main {
 
 
     // Alg2
-    public static void ordena2(int[] v, int tam) {
-        int k;
-        int n = tam;
-        for (k = n / 2; k >= 1; k--) {
-            f(v, k, n);
+    fun ordena2(v: IntArray, tam: Int) {
+        var k: Int
+        val n = tam
+        k = n / 2
+        while (k >= 1) {
+            f(v, k, n)
+            k--
         }
-        k = n;
+        k = n
         while (k > 1) {
-            g(v, 1, k--);
-            f(v, 1, k);
+            g(v, 1, k--)
+            f(v, 1, k)
         }
     }
 
-    private static void f(int[] v, int k, int n) {
+    private fun f(v: IntArray, k: Int, n: Int) {
+        var k = k
         while (2 * k <= n) {
-            int j = 2 * k;
+            var j = 2 * k
             if ((j < n) && (v[j - 1] < v[j])) {
-                j++;
+                j++
             }
             if (v[k - 1] >= v[j - 1]) {
-                break;
+                break
             }
-            g(v, k, j);
-            k = j;
+            g(v, k, j)
+            k = j
         }
     }
 
-    private static void g(int[] v, int i, int j) {
-        int temp = v[i - 1];
-        v[i - 1] = v[j - 1];
-        v[j - 1] = temp;
+    private fun g(v: IntArray, i: Int, j: Int) {
+        val temp = v[i - 1]
+        v[i - 1] = v[j - 1]
+        v[j - 1] = temp
     }
 
 
     // Alg3
-    public static void ordena3(int[] v, int tam) {
-        int m = h(v, tam);
-        int[] c = new int[m + 1];
-        int[] w = new int[tam];
-        for (int i = 0; i < tam; i++) {
-            c[v[i]] = c[v[i]] + 1;
+    fun ordena3(v: IntArray, tam: Int) {
+        val m = h(v, tam)
+        val c = IntArray(m + 1)
+        val w = IntArray(tam)
+        for (i in 0 until tam) {
+            c[v[i]] = c[v[i]] + 1
         }
-        for (int i = 1; i < m + 1; i++) {
-            c[i] = c[i] + c[i - 1];
+        for (i in 1 until m + 1) {
+            c[i] = c[i] + c[i - 1]
         }
-        for (int i = tam - 1; i >= 0; i--) {
-            w[c[v[i]] - 1] = v[i];
-            c[v[i]] = c[v[i]] - 1;
+        for (i in tam - 1 downTo 0) {
+            w[c[v[i]] - 1] = v[i]
+            c[v[i]] = c[v[i]] - 1
         }
-        for (int i = 0; i < tam; i++) {
-            v[i] = w[i];
+        for (i in 0 until tam) {
+            v[i] = w[i]
         }
     }
 
-    private static int h(int[] v, int tam) {
-        int i;
-        int m = v[0];
-        for (i = 1; i < tam; i++) {
+    private fun h(v: IntArray, tam: Int): Int {
+        var m = v[0]
+        var i = 1
+        while (i < tam) {
             if (v[i] > m) {
-                m = v[i];
+                m = v[i]
             }
+            i++
         }
-        return m;
+        return m
     }
 
 
-//Fisher Yates
-
-    public static void aleatorio(int[] v, int tam) {
-        int i;
-        for (i = 0; i < tam; i++) {
-            v[i] = i;
+    //Fisher Yates
+    fun aleatorio(v: IntArray, tam: Int) {
+        var i = 0
+        while (i < tam) {
+            v[i] = i
+            i++
         }
-        for (i = tam - 1; i > 0; i--) {
-// random.nextInt(k) devuelve un entero pseudoaleatorio distribuido
+        i = tam - 1
+        while (i > 0) {
+            // random.nextInt(k) devuelve un entero pseudoaleatorio distribuido
 // uniformemente entre 0 (incluido) y el valor especificado k (excluido)
-            int j = random.nextInt(i + 1);
-            int temp = v[i];
-            v[i] = v[j];
-            v[j] = temp;
+            val j = random.nextInt(i + 1)
+            val temp = v[i]
+            v[i] = v[j]
+            v[j] = temp
+            i--
         }
     }
-
 }
 
